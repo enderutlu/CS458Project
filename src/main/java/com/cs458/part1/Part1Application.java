edb50a7b422273d177c32ea6ee299cda7624b398
Ender <endeerutlu@gmail.com>
2025-03-02 15:17:59 +0300

    oauth and authentication

--- src/main/java/com/cs458/part1/Part1Application.java
+++ src/main/java/com/cs458/part1/Part1Application.java
@@ -1,46 +1,13 @@
 package com.cs458.part1;
 
-import java.time.LocalDate;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-
-
-@RestController
 @SpringBootApplication
+@EnableWebSecurity
 public class Part1Application {
-	@Autowired
-	UsersRepository repo;
 
-	@GetMapping("getTest")
-	public List<Users> get() {
-		return repo.findAll().stream().collect(Collectors.toList());
-	}
-	
-	@GetMapping("insertTest")
-	public String insert() {
-		Users user = new Users();
-		user.setId(1);
-		user.setEmail("ender@gmail.com");
-		user.setName("Ender");
-		user.setSurname("Utlu");
-		user.setPhoneNumber("05448242002");
-		user.setDateOfBirth(LocalDate.of(2002, 8, 1));
-		return repo.save(user).toString();
-	}
-	
-	@DeleteMapping("deleteTest/{id}")
-	public void delete(@PathVariable int id) {
-		repo.deleteById(id);
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(Part1Application.class, args);
 	}
--- src/main/java/com/cs458/part1/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cs458.part1;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsersRepository extends JpaRepository<Users, Integer>{
-    
-}
